4cfbafb8ad2817ba56fbdbf2bfe5a607c3ed854d
음지훈 <jhmc5268@naver.com>
2023-10-05 23:18:12 +0900

    Design: Layout 컴포넌트 위치 작업

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,10 +1,20 @@
 import { Outlet } from 'react-router-dom'
+import styled from 'styled-components'
 
 const Layout = () => {
   return (
-     <Outlet/>  
+    <MainCard>
+       <Outlet/>
+    </MainCard>  
   )
 }
 
 export default Layout
 
+const MainCard = styled.main`
+  width: 100%;
+  min-height: 67.5rem;
+  ${({theme}) => theme.FlexCol};
+  ${({theme}) => theme.FlexCenter};
+  background-color: ${({theme}) => theme.bgColor};
+`;
